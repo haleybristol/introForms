0c45db8332d18015f4b077d19dd7826db08eb902
haley <student@epicodus.com>
2016-01-15 10:48:15 -0800

    add working jquery

--- js/script.js
+++ js/script.js
@@ -3,15 +3,24 @@ $(document).ready(function(){
     var landtype = $("input#landtype").val();
     var risk = $("input#risk").val();
     var age = $("input#age").val();
+    var party = $("input#party").val();
+    var exotic = $("input#exotic").val();
+      console.log
 
-    if (landtype === "yes" && risk === "yes" && age != ""){
+    if (landtype === "yes" && risk === "yes" && age != "" && party === "yes" && exotic != ""){
       $(".australia").show();
       $(".switzerland").hide();
-    } else if (landtype === "no" && risk === "yes" && age != ""){
-      $("#australia").hide();
-      $("#switzerland").show();
+      $(".florida").hide();
+    } else if (landtype === "no" && risk === "yes" && age != "" && party === "no" && exotic != ""){
+      $(".australia").hide();
+      $(".switzerland").show();
+      $(".florida").hide();
+    } else if (landtype === "yes" && risk === "no" && age !="" && party === "no" && exotic != ""){
+      $(".australia").hide();
+      $(".switzerland").hide();
+      $(".florida").show();
     } else {
-      alert("Please complete the form.")
+      alert("Please complete the form.");
     }
   event.preventDefault();
   });
