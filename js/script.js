ca500a4fb6f3ad0517e22b67225880e15d2de4a2
haley <student@epicodus.com>
2016-01-15 14:50:53 -0800

    ended jquery function

--- js/script.js
+++ js/script.js
@@ -2,24 +2,26 @@ $(document).ready(function(){
   $("form").submit(function(event){
     var landtype = $("input#landtype").val();
     var risk = $("input#risk").val();
-    var age = $("input#age").val();
-    var party = $("input#party").val();
     var exotic = $("input#exotic").val();
+    var party = $("input#party").val();
+    var dolphin = $("input#dolphin").val();
 
-    if (landtype === "yes" && risk === "yes" && age != "" && party === "yes" && exotic != ""){
+    if (landtype === "yes" && risk === "yes" && exotic != "" && party === "yes" && dolphin != ""){
       $(".australia").show();
       $(".switzerland").hide();
       $(".florida").hide();
-    } else if (landtype === "no" && risk === "yes" && age != "" && party === "no" && exotic != ""){
+    } else if (landtype === "no" && risk === "yes" && exotic != "" && party === "no" && exotic != ""){
       $(".australia").hide();
       $(".switzerland").show();
       $(".florida").hide();
-    } else if (landtype === "yes" && risk === "no" && age !="" && party === "no" && exotic != ""){
+    } else if (landtype === "yes" && risk === "no" && exotic !="" && party === "no" && dolphin != ""){
       $(".australia").hide();
       $(".switzerland").hide();
       $(".florida").show();
     } else {
-      alert("Please complete the form.");
+      $(".australia").hide();
+      $(".switzerland").show();
+      $(".florida").hide();
     }
   event.preventDefault();
   });
